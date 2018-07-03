dd6a363a074805dcdd954b1b385f1354b3294d52
wnyao <wenyao_k@hotmail.com>
2018-07-03 10:08:44 +0800

    Apply prettier formatter

--- src/board.js
+++ src/board.js
@@ -1,51 +1,53 @@
-import React from 'react';
-import './css/index.css';
+import React from "react";
+import "./css/index.css";
 
 //Presentational Component
 const Button = props => (
-    <button
-        className={props.className}
-        onClick={props.onClick}
-    >
-        {props.value}
-    </button>
+  <button className={props.className} onClick={props.onClick}>
+    {props.value}
+  </button>
 );
 
 class Board extends React.Component {
-    //Return a single button component
-    renderSquare(i, coordinate) {
-        return (
-            <Button 
-                className="square"
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i, coordinate)}
-            />
-        );
-    }
+  //Return a single button component
+  renderSquare(i, coordinate) {
+    return (
+      <Button
+        className="square"
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i, coordinate)}
+      />
+    );
+  }
 
-    //Render board of tictactoe
-    renderBoard() {
-        const rowBlock = (squares, key) => (<div key={key} className="board-row">{squares[0]}{squares[1]}{squares[2]}</div>);
-        const length = 3;
+  //Render board of tictactoe
+  renderBoard() {
+    const rowBlock = (squares, key) => (
+      <div key={key} className="board-row">
+        {squares[0]}
+        {squares[1]}
+        {squares[2]}
+      </div>
+    );
+    const length = 3;
 
-        const boardRows = Array.from({length}, (currentVal, outer) => { //outer & index: index of the current element being processed in the array
-            const row = 3 - outer;
-            const boardRow = Array.from({length}, (currentVal, index) => ( //currentVal: current element being processed
-                this.renderSquare(outer * length + index, [row, index + 1])
-            ))
+    const boardRows = Array.from({ length }, (currentVal, outer) => {
+      //outer & index: index of the current element being processed in the array
+      const row = 3 - outer;
+      const boardRow = Array.from({ length }, (
+        currentVal,
+        index //currentVal: current element being processed
+      ) => this.renderSquare(outer * length + index, [row, index + 1]));
 
-            return rowBlock(boardRow, outer + 1);
-        });
-        return (boardRows);
-    }
+      return rowBlock(boardRow, outer + 1);
+    });
+    return boardRows;
+  }
 
-    render() {
-        const boardComponent = this.renderBoard();
-        return(boardComponent);
-    }
+  render() {
+    const boardComponent = this.renderBoard();
+    return boardComponent;
+  }
 }
 
-export {
-    Button,
-    Board
-}
+export { Button, Board };
