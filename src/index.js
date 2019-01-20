c2d2b006477e58bae0c58dcc66f054902934212f
WenYaoKong <wenyao@cyder.com.sg>
2019-01-20 13:53:01 +0800

    Add hot reloading

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GameContainer from './js/gameContainer.js';
 
-ReactDOM.render(<GameContainer />, document.getElementById('root'));
+const renderer = () =>
+  ReactDOM.render(<GameContainer />, document.getElementById('root'));
+
+// Hot Module Replacement
+if (module.hot) module.hot.accept(Component => renderer());
+
+// Initial load
+renderer();
